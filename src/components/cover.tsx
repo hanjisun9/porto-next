e43ac79336e19ed205f38df70e7246df6e54e3f2
jujuu <azizahnf85@gmail.com>
2025-09-02 02:22:29 +0700

    update

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -1,50 +1,37 @@
 "use client";
 
 import { useEffect } from "react";
+import Image from "next/image";
 
 export default function Cover() {
     useEffect(() => {
         const music = document.getElementById("bg-music") as HTMLAudioElement | null;
-        const coverRight = document.getElementById("cover-right");
+        const coverFront = document.getElementById("cover-front");
         let isPlaying = false;
 
-        if (music && coverRight) {
-            coverRight.addEventListener("click", () => {
-                // klik cover kanan → buka + play/pause musik
-                coverRight.classList.toggle("turn");
-
+        if (music && coverFront) {
+            coverFront.addEventListener("click", () => {
                 if (isPlaying) {
                     music.pause();
                 } else {
                     music.play();
                 }
                 isPlaying = !isPlaying;
-
-                // setelah terbuka, turunkan z-index cover kanan
-                setTimeout(() => {
-                    if (coverRight.classList.contains("turn")) {
-                        coverRight.style.zIndex = "-1";
-                    } else {
-                        coverRight.style.zIndex = "100"; // kalau ditutup lagi
-                    }
-                }, 1000); // sesuai durasi animasi
             });
         }
     }, []);
 
     return (
         <>
-            {/* Cover kiri */}
-            <div className="cover cover-left">
-                <audio id="bg-music" loop>
-                    <source src="/rainbow.mp3" type="audio/mpeg" />
-                </audio>
-                <span></span>
-            </div>
+            <div className="wrapper">
+                <div className="cover cover-left">
+                    <audio id="bg-music" loop>
+                        <source src="/rainbow.mp3" type="audio/mpeg" />
+                    </audio>
+                </div>
 
-            {/* Cover kanan */}
-            <div className="cover cover-right" id="cover-right">
-                <span></span>
+                <div className="cover cover-right">
+                </div>
             </div>
         </>
     );
